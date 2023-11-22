3070cc9ff086ba32099ff08ea240090851a6702a
Rodrigo Velasco <88783591+RoRoVR@users.noreply.github.com>
2023-11-22 11:42:34 -0400

    commid desde usuario

---
+++ SQL.sql
@@ -0,0 +1,2 @@
+BACKUP DATABASE [DWConfiguration] TO  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup\DWConfiguration.bak' WITH NOFORMAT, NOINIT,  NAME = N'DWConfiguration-Completa Base de datos Copia de seguridad', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+GO
